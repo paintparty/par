3cfecdb572aa88d922e1d77c1da12903d4085dbc
Jeremiah Coyle <jcoyle@gmail.com>
2020-06-05 14:12:48 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject paintparty/par "0.1.0-SNAPSHOT"
+(defproject paintparty/par "0.1.2"
   :description "Print-and-return macros for Clojure(Script)"
-  :url "http://github.com/paintparty/par"
+  :url "https://github.com/paintparty/par"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns par.core}
-  :deploy-repositories [["clojars"  {:sign-releases false
-                                     :url "https://clojars.org/paintparty/par"
-                                     :username :env/CLOJARS_USERNAME
-                                     :password :env/CLOJARS_PASSWORD}]])
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://repo.clojars.org"}]])
