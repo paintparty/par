2508dbaf49dce6c2f7b5de09b0b6ca9cd77c5e9f
Jeremiah Coyle <jcoyle@gmail.com>
2022-02-12 13:49:27 -0800

    Upgrade minor version
    Upgrade clojure version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.paintparty/par "1.0.0"
+(defproject org.clojars.paintparty/par "1.1.0"
   :description "Print-and-return macros for Clojure(Script)"
   :url "https://github.com/paintparty/par"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [io.aviso/pretty "1.1"]
                  [zprint "1.2.0"]]
   :repl-options {:init-ns par.core}
