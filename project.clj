ede41ccb98fe8b04beeb890349174d4fb34fe941
Jeremiah Coyle <jcoyle@gmail.com>
2023-01-18 00:38:41 -0800

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.paintparty/par "1.1.0"
+(defproject org.clojars.paintparty/par "2.0.0"
   :description "Print-and-return macros for Clojure(Script)"
   :url "https://github.com/paintparty/par"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
